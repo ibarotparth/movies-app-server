e796f8dc92328857f3e0121ae62664f2279e6f4c
Parth <pbarot297@gmail.com>
2024-03-04 13:55:58 -0800

    Update Configure file

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,8 +8,8 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    //paste your MongoDB URI String from MongoDB Atlas in below line
-    try app.databases.use(.mongo(connectionString: "MONGODB URI STRING"), as: .mongo)
+    //paste your MongoDB URI String from MongoDB Atlas in below line with your username and password
+    try app.databases.use(.mongo(connectionString: "mongodb+srv://<username>:<password>@moviescluster.n2xf6zt.mongodb.net/moviesdb?retryWrites=true&w=majority&appName=MoviesCluster"), as: .mongo)
 
     // register controllers
     try app.register(collection: MoviesController())
